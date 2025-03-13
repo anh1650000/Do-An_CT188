01b448978fd6851a388c688bfc607ab18b0f22d3
Anh165000 <145004469+Anh165000@users.noreply.github.com>
2025-03-13 16:56:39 +0700

    first commit

---
+++ product-detail_script.js
@@ -0,0 +1,447 @@
+const products = [{
+    "pId": "sp001",
+        "name": "Bến Tre-Vĩnh Long-Cần Thơ. Sắc xanh nơi hạ nguồn Mê Kông | Bến Tre - “Nghe dòng sông kể chuyện” - Vĩnh Long - Thưởng thức ẩm thực tiêu biểu Việt Nam - Cần Thơ - Nghỉ dưỡng resort tương đương 4 sao",
+        "time": 3,
+        "price": 3990000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Bến Tre - Vĩnh Long - Cần Thơ",
+            "cuisine": "Buffet sáng, Theo thực đơn, Đặc sản địa phương",
+            "object": "Cặp đôi, Gia đình nhiều thế hệ, Thanh niên",
+            "ideal time": "Quanh năm",
+            "transport": "Xe du lịch",
+            "promotion": "Đã bao gồm ưu đãi trong giá tour"
+        },
+        schedule:{
+            day1:{
+                "title": "Thành phố Hồ Chí Minh- Bến Tre \"Xanh hơn trên mỗi dòng sông\" ",
+                "description":""
+            },
+            day2:{
+                "title": "Bến Tre - Vĩnh Long - Nhà gốm - Nhà dừa - Cần Thơ - Cần Thơ Eco Resort ",
+                "description":""
+            },
+            day3:{
+                "title": "Cần Thơ - Chợ nổi Cái Răng - Thành phố Hồ Chí Minh ",
+                "description":""
+            }
+        }
+    },
+    {"pId": "sp002",
+        "name": "Cao Bằng - Thác Bản Giốc - Ba Bể",
+        "time": 3,
+        "price": 3290000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Khu di tích Pắc Bó, Thác Bản Giốc, Động Ngườm Ngao,...",
+            "cuisine": "Đặc sản địa phương",
+            "object": "Gia đình nhiều thế hệ",
+            "ideal time": "Quanh năm",
+            "transport": "Xe du lịch",
+            "promotion": "Đã bao gồm ưu đãi trong giá tour"
+        },
+        schedule:{
+            day1:{
+                "title": "Hà Nội - Khu di tích Pắc Bó",
+                "description":""
+            },
+            day2:{
+                "title": "Thác Bản Giốc - Động Ngườm Ngao",
+                "description":""
+            },
+            day3:{
+                "title": "Vườn quốc gia Ba Bể - Hà Nội",
+                "description":""
+            }
+        }
+    },
+    {"pId": "sp003",
+        "name": "BĐà Lạt - Muôn Ngàn Trải Nghiệm: Thác Bobla - Dapa Hill - Thung Lũng Tình Yêu - Chinh Phục Cầu Kính Ngàn Thông 7D",
+        "time": 3,
+        "price": 2690000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Đà Lạt, Thác Bobla, Ga Xe Lửa, Thung Lũng Tình Yêu, Cầu Kính 7D",
+            "cuisine": "Buffet sáng, Theo thực đơn",
+            "object": "Cặp đôi, Gia đình nhiều thế hệ, Thanh niên, Trẻ em",
+            "ideal time": "Quanh năm",
+            "transport": "Xe du lịch",
+            "promotion": "Đã bao gồm ưu đãi trong giá tour"
+        },
+        schedule:{
+            day1:{
+                "title": "Thành phố Hồ Chí Minh- Đà Lạt \"Thành phố ngàn hoa\" ",
+                "description":""
+            },
+            day2:{
+                "title": "Thung Lũng Tình Yêu - Chinh Phục Cầu Kính Ngàn Thông 7D",
+                "description":""
+            },
+            day3:{
+                "title": "Đà Lạt - Dapa Hill - Thành phố Hồ Chí Minh",
+                "description":""
+            }
+        }
+    },
+    {"pId": "sp004",
+        "name": "Lạc bước xứ sở ngàn mây| Hà Giang - Lũng Cú - Đồng Văn - Sapa - Fansipan",
+        "time": 7,
+        "price": 10890000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Hà Giang, Lũng Cú, Đồng Văn, Sapa, Fansipan",
+            "cuisine": "Buffet sáng, Theo thực đơn",
+            "object": "Cặp đôi, Gia đình nhiều thế hệ, Thanh niên",
+            "ideal time": "Quanh năm",
+            "transport": "Máy bay, Xe du lịch",
+            "promotion": "Đã bao gồm ưu đãi trong giá tour"
+        },
+        schedule:{
+            day1:{
+                "title": "Hà Nội - Hà Giang",
+                "description":""
+            },
+            day2:{
+                "title": "Hà Giang - Quản Bạ - Yên Minh - Đồng Văn",
+                "description":""
+            },
+            day3:{
+                "title": "Đồng Văn - Mèo Vạc - Sông Nho Quế - Tp. Hà Giang",
+                "description":""
+            },
+            day4:{
+                "title": "Hà Giang - \"Thị trấn trong sương\" Sapa",
+                "description":""
+            },
+            day5:{
+                "title": "Sapa - Khám phá \"Nóc nhà Đông Dương\" Fansipan",
+                "description":""
+            },
+            day6:{
+                "title": "Sapa - Tp.Lào Cai - Sân bay Nội Bài",
+                "description":""
+            },
+            day7:{
+                "title": "Hà Nội - Kết thúc chương trình",
+                "description":""
+            }
+        }
+    },
+    {"pId": "sp005",
+        "name": "Miền Bắc: Hạ Long - Vịnh Lan Hạ - Cát Bà - Vũ Điệu biển khơi ( Trải nghiệm du thuyền đằng cấp Paradise Grand & M-Gallery Hotel Perle D’Orient )",
+        "time": 4,
+        "price": 39990000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Quảng Ninh, vịnh Lan Hạ, Cát Bà",
+            "cuisine": "Buffet sáng, Theo thực đơn",
+            "object": "Cặp đôi, Gia đình nhiều thế hệ, Thanh niên",
+            "ideal time": "Quanh năm",
+            "transport": "Máy bay, Xe du lịch",
+            "promotion": "Ưu đãi trực tiếp vào giá tour"
+        },
+        schedule:{
+            day1:{
+                "title": "TP.HCM - SÂN BAY CÁT BI - HẠ LONG - SHOW \"VŨ ĐIỆU BIỂN KHƠI\" (Ăn tối)",
+                "description":""
+            },
+            day2:{
+                "title": "CHILL CÙNG GIÓ BIỂN, NẮNG & HOÀNG HÔN TRÊN VỊNH LAN HẠ (Ăn ba bữa)",
+                "description":""
+            },
+            day3:{
+                "title": "VƯỜN QUỐC GIA CÁT BÀ - CHINH PHỤC ĐỈNH TRÊN ĐẢO CÁT BÀ (Ăn ba bữa)",
+                "description":""
+            },
+            day4:{
+                "title": "KHÁM PHÁ TRẢI NGHIỆM CAO CẤP TẠI RESORT - CÁP TREO SUN WORLD (Ăn sáng)",
+                "description":""
+            }
+        }
+    },
+    {"pId": "sp006",
+        "name": "Đà Nẵng - KDL Bà Nà- Đầm Lập An - Phố cổ Hội An - Huế - Làng hương Thuỷ Xuân",
+        "time": "4",
+        "price": 8990000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Đà Nẵng, Bà Nà, Phố cổ Hội An, Huế",
+            "cuisine": "Theo thực đơn, Đặc sản địa phương",
+            "object": "Người lớn tuổi, Cặp đôi, Gia đình nhiều thế hệ, Thanh niên, Trẻ em",
+            "ideal time": "Quanh năm",
+            "transport": "Máy bay, Xe du lịch",
+            "promotion": "Đã bao gồm ưu đãi trong giá tour"
+        },
+        schedule:{
+            day1:{
+                "title": "Cần Thơ - Đà Nẵng - Huế",
+                "description":""
+            },
+            day2:{
+                "title": "Huế - Đại Nội  - Làng hương Thủy Xuân - Đầm Lập An",
+                "description":""
+            },
+            day3:{
+                "title": "Đã Nẵng - KDL Bà Nà - Phố cổ Hội An",
+                "description":""
+            },
+            day4:{
+                "title": "Đã Nẵng - Bán đảo Sơn Trà - Viếng chùa Linh Ứng - Cần Thơ",
+                "description":""
+            }
+        }
+    },
+    {"pId": "sp007",
+        "name": "Mùa hoa Tây Bắc: Hòa Bình - Mộc Châu - Điện Biên - Lai Châu - Sapa - Fansipan - Yên Bái - Hà Nội | Tặng vé tàu hỏa leo núi Mường Hoa",
+        "time": 7,
+        "price": 10690000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Tây Bắc, Hòa Bình, Mộc Châu, Điện Biên,...",
+            "cuisine": "Theo thực đơn, Đặc sản địa phương",
+            "object": "Cặp đôi, Gia đình nhiều thế hệ, Thanh niên",
+            "ideal time": "Quanh năm",
+            "transport": "Máy bay, Xe du lịch",
+            "promotion": "Đã bao gồm ưu đãi trong giá tour"
+        },
+        schedule:{
+            day1:{
+                "title": "Tp.HCM - Hà Nội - Đèo Thung Khe - Mộc Châu Island",
+                "description":""
+            },
+            day2:{
+                "title": "Mộc Châu - Tp. Sơn La - Điện Biên",
+                "description":""
+            },
+            day3:{
+                "title": "Điện Biên - Lai Châu",
+                "description":""
+            },
+            day4:{
+                "title": "Lai Châu - Sapa",
+                "description":""
+            },
+            day5:{
+                "title": "Sapa - Mù Cang Chải - Nghĩa Lộ",
+                "description":""
+            },
+            day6:{
+                "title": "Nghĩa Lộ - Yên Bái - Hà Nội",
+                "description":""
+            },
+            day7:{
+                "title": "Hà Nội - Tp.HCM",
+                "description":""
+            }
+        }
+    },
+    {"pId": "sp008",
+        "name": "Phú Quốc - Chinh Phục Đệ Nhất Tứ Đảo - Trải Nghiệm Đi Bộ Dưới Đáy Biển - Thị Trấn Hoàng Hôn - Kiss Bridge - Bãi Sao",
+        "time": 3,
+        "price": 5190000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Phú Quốc, Tour 4 đảo, Bãi sao, Thị trấn hoàng hôn",
+            "cuisine": "Theo thực đơn",
+            "object": "Người lớn tuổi, Cặp đôi, Gia đình nhiều thế hệ, Thanh niên, Trẻ em",
+            "ideal time": "Quanh năm",
+            "transport": "Máy bay, Xe du lịch",
+            "promotion": "Đã bao gồm ưu đãi trong tour"
+        },
+        schedule:{
+            day1:{
+                "title": "Thành phố Hồ Chí Minh - Phú Quốc",
+                "description":""
+            },
+            day2:{
+                "title": "Chinh phục đệ nhất tử Đảo - Trải nghiệm đi bộ dưới đáy biển - Thị trấn Hoàng Hôn",
+                "description":""
+            },
+            day3:{
+                "title": "Phú Quốc - Bãi Sao - Thành phố Hồ Chí Minh",
+                "description":""
+            }
+        }
+    },
+    {"pId": "sp009",
+        "name": "Bangkok - Pattaya - Wat Arun - Ngắm Hoàng Hôn Trên Du Thuyền Sông Chao Phraya - Thưởng Thức Calypso Cabaret Show",
+        "time": 5,
+        "price": 8290000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Bangkok - Pattaya - Chùa Bình Minh Wat Arun",
+            "cuisine": "Buffet sáng, Theo thực đơn, Đặc sản địa phương",
+            "object": "Gia đình nhiều thế hệ",
+            "ideal time": "Quanh năm",
+            "transport": "Máy bay, Xe du lịch",
+            "promotion": "Theo các chương trình ưu đãi của công ty"
+        },
+        schedule:{
+            day1:{
+                "title": "Bangkok - Pattaya",
+                "description":""
+            },
+            day2:{
+                "title": "Bãi biển Jomtien - Chao Chee Chan",
+                "description":""
+            },
+            day3:{
+                "title": "Pattaya - Bangkok - Calypso Cabaret Show",
+                "description":""
+            },
+            day4:{
+                "title": "Chùa Wat Arun - Đền Phật Vàng Bốn Mặt - Jodd Fair",
+                "description":""
+            },
+            day5:{
+                "title": "Bangkok - Hà Nội",
+                "description":""
+            }
+        }
+    },
+    {"pId": "sp010",
+        "name": "Tây Âu: Pháp - Bỉ - Hà Lan - Luxembourg - Thụy Sĩ - Đức (Lễ hội hoa Tulip Keukenhof)",
+        "time": "9",
+        "price": 60990000,
+        images:{
+            "img1": "",
+            "img2": "",
+            "img3": "",
+            "img4": "",
+            "img5": "",
+            "img6": "",
+        },
+        moreInformation:{
+            "tourist attractions": "Paris, Đuc, Hà Lan, Amsterdam, Thuy Si, Luxembourg",
+            "cuisine": "Buffet sáng, Theo thực đơn, Đặc sản địa phương",
+            "object": "Cặp đôi, Thanh niên, Gia đình nhiều thế hệ",
+            "ideal time": "Quanh năm",
+            "transport": "Máy bay",
+            "promotion": "Đã bao gồm ưu đãi trong giá tour"
+        },
+        schedule:{
+            day1:{
+                "title": "Tp.Hồ Chí Minh - Paris",
+                "description":""
+            },
+            day2:{
+                "title": "Paris",
+                "description":""
+            },
+            day3:{
+                "title": "Paris - Brussels - Amsterdam",
+                "description":""
+            },
+            day4:{
+                "title": "Antwerpen - Vườn hoa Keukenhof - Amsterdam",
+                "description":""
+            },
+            day5:{
+                "title": "Amsterdam Cologne - Luxembourg - Belval",
+                "description":""
+            },
+            day6:{
+                "title": "Belval - Strasbourg - Zurich",
+                "description":""
+            },
+            day7:{
+                "title": "Zurich - Stuttgart -Frankurt",
+                "description":""
+            },
+            day8:{
+                "title": "Frankurt - Tp.HCM",
+                "description":""
+            },
+            day9:{
+                "title": "Tp.HCM",
+                "description":""
+            }
+        }
+    },
+];
+
+const params = new URLSearchParams(window.location.search);
+const productId = params.get('product-id');
+productId = productId - 1;
+console.log(productId);
+if(products[productId] != null){
+    document.getElementById("product-price").textContent = products[productId].price + " ₫/Khách";
+    for(let i = 0; i < products[productId].images.length; i++){
+        document.getElementById("product-img" + (i+1)).src = products[productId].images["img" + (i+1)]; 
+    }
+    document.getElementsByClassName("tourist-attractions__description").textContent = "";
+    document.getElementsByClassName("tourist-attractions__description").textContent = products[productId].moreInformation["tourist attractions"];
+    document.getElementsByClassName("cuisine__description").textContent = products[productId].moreInformation["cuisine"];
+    document.getElementsByClassName("object__description").textContent = products[productId].moreInformation["object"];
+    document.getElementsByClassName("ideal-time__description").textContent = products[productId].moreInformation["ideal time"];
+    document.getElementsByClassName("transport__description").textContent = products[productId].moreInformation["transport"];
+    document.getElementsByClassName("promotion__description").textContent = products[productId].moreInformation["promotion"];
+    // for(let i = 0; i < products[productId].schedule.length; i++){
+    //     document.getElementsByClassName("schedule__day" + (i+1)).textContent = products[productId].schedule["day" + (i+1)].title;
+    // }
+}else{
+    document.querySelector('.article__product-detail').innerHTML = "<h1>Không tìm thấy sản phẩm</h1>";
+} 
